3e6d78ebe6e656f5aea8ae78b7c41cd55709096c
Josue Sanchez Nima <josue.sanchez.n1402@hotmail.com>
2023-07-24 02:01:36 -0500

    Sección de "Studio"

--- src/app/components/studio-section/studio-section.component.ts
+++ src/app/components/studio-section/studio-section.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'oc-studio-section',
-  templateUrl: './studio-section.component.html',
-  styleUrls: ['./studio-section.component.scss']
+	selector: 'oc-studio-section',
+	templateUrl: './studio-section.component.html',
+	styleUrls: ['./studio-section.component.scss'],
 })
-export class StudioSectionComponent {
+export class StudioSectionComponent implements OnInit {
+	sliderImages = [
+		'assets/images/slider_1.webp',
+		'assets/images/slider_2.webp',
+		'assets/images/slider_3.webp',
+		'assets/images/slider_4.webp',
+		'assets/images/slider_5.webp',
+		'assets/images/slider_6.webp',
+		'assets/images/slider_7.webp',
+		'assets/images/slider_8.webp',
+	];
 
+	currentImageIndex = 0;
+
+	ngOnInit() {
+		this.startSliderAnimation();
+	}
+
+	startSliderAnimation() {
+		setInterval(() => {
+			this.currentImageIndex =
+				(this.currentImageIndex + 1) % this.sliderImages.length;
+		}, 1500);
+	}
 }
