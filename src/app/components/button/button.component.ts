ea3091567a072eedad9d3155f04849cc09b83adf
Josue Sanchez Nima <josue.sanchez.n1402@hotmail.com>
2023-07-24 00:26:00 -0500

    botones

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'oc-button',
-  templateUrl: './button.component.html',
-  styleUrls: ['./button.component.scss']
+	selector: 'oc-button',
+	templateUrl: './button.component.html',
+	styleUrls: ['./button.component.scss'],
 })
 export class ButtonComponent {
-
+	@Input() text: string = '';
+	@Input() type: string = 'primary';
 }
