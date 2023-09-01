e4ce7d8f27fc54adb58b2b299e8445d7aea4a096
Josue Sanchez Nima <josue.sanchez.n1402@hotmail.com>
2023-08-31 23:17:52 -0500

    Selector de lenguaje desplegable

--- src/app/components/footer/language-selector/language-selector.component.ts
+++ src/app/components/footer/language-selector/language-selector.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
-  selector: 'oc-language-selector',
-  templateUrl: './language-selector.component.html',
-  styleUrls: ['./language-selector.component.scss']
+	selector: 'oc-language-selector',
+	templateUrl: './language-selector.component.html',
+	styleUrls: ['./language-selector.component.scss'],
 })
 export class LanguageSelectorComponent {
+	@ViewChild('selector') selectorRef!: ElementRef;
+	@ViewChild('dropdown') dropDowntRef!: ElementRef;
 
+	toggleOptionsVisibility() {
+		const selectorElement: HTMLElement = this.selectorRef.nativeElement;
+		const dropDowntElement: HTMLElement = this.dropDowntRef.nativeElement;
+		selectorElement.classList.toggle('active');
+		dropDowntElement.classList.toggle('active');
+	}
 }
